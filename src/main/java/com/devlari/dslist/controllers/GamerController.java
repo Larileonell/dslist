41f00020fce406e486631f3459f8b2df1032d11c
Allana Larissa <larissaleonel98@gmail.com>
2023-05-09 16:58:53 -0300

    Game repository, Service and Controller

---
+++ src/main/java/com/devlari/dslist/controllers/GamerController.java
@@ -0,0 +1,23 @@
+package com.devlari.dslist.controllers;
+
+import com.devlari.dslist.dto.GameMinDTO;
+import com.devlari.dslist.entities.Game;
+import com.devlari.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GamerController {
+    @Autowired
+    private GameService gameService;
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
